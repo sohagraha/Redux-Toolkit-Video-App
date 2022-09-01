96324dc6f542d54d385a9dee4d98d018bf0ce4cf
sohagraha <sohagrahabd@gmail.com>
2022-09-01 08:50:58 +0600

    feat : dynamic videos show on homepage

--- src/components/videoGrid/videoGridItem/VideoGridItem.js
+++ src/components/videoGrid/videoGridItem/VideoGridItem.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const VideoGridItem = ({ video }) => {
+const VideoGridItem = ({ video = {} }) => {
+    const { id, thumbnail, title, duration, avatar, author, views, date } = video;
     return (
         <div
             className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]"
         >
             <div className="w-full flex flex-col">
                 <div className="relative">
-                    <Link to="video/1">
+                    <Link to={`videos/>${id}`}>
                         <img
-                            src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
+                            src={thumbnail}
                             className="w-full h-auto"
                             alt="Some video title"
                         />
@@ -19,34 +20,34 @@ const VideoGridItem = ({ video }) => {
                     <p
                         className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py"
                     >
-                        12:10
+                        {duration}
                     </p>
                 </div>
 
                 <div className="flex flex-row mt-2 gap-2">
-                    <Link to="video/2">
+                    <Link to={`videos/>${id}`}>
                         <img
-                            src="https://avatars.githubusercontent.com/u/73503432?v=4"
+                            src={avatar}
                             className="rounded-full h-6 w-6"
-                            alt="Learn with Sumit"
+                            alt={author}
                         />
                     </Link>
 
-                    <div clas="flex flex-col">
-                        <Link to="video/1">
+                    <div className="flex flex-col">
+                        <Link to={`videos/>${id}`}>
                             <p
                                 className="text-slate-900 text-sm font-semibold"
                             >
-                                Video title
+                                {title}
                             </p>
                         </Link>
                         <Link className="text-gray-400 text-xs mt-2 hover:text-gray-600"
-                            to="video/1"
+                            to={`videos/>${id}`}
                         >
-                            Learn with Sumit
+                            {author}
                         </Link>
                         <p className="text-gray-400 text-xs mt-1">
-                            200 views . May 3, 2022
+                            {views} views . {date}
                         </p>
                     </div>
                 </div>
