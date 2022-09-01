92169b4628411ca59ccfd9404e44684c8904f0d8
sohagraha <sohagrahabd@gmail.com>
2022-09-02 01:26:42 +0600

    feat : dynamic pagination with design

--- src/components/ui/pagination/Pagination.js
+++ src/components/ui/pagination/Pagination.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { pageChanged } from '../../../features/filter/filterSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
 const Pagination = () => {
+    const dispatch = useDispatch();
     const { videos } = useSelector((state) => state.videos);
+    const { page } = useSelector((state) => state.filter);
+
+    const active = "bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer";
+    const inactive = "bg-gray-200 text-gray-600 px-4 py-1 rounded-full cursor-pointer";
+
     const pages = Math.ceil(videos.length / 4);
     const range = [];
     for (let i = 1; i <= pages; i++) {
         range.push(i);
+    }
+    const handlePage = (pagee) => {
+        dispatch(pageChanged(pagee))
     }
     return (
         <section className="pt-12">
@@ -14,9 +24,9 @@ const Pagination = () => {
                 className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 justify-end"
             >
                 {
-                    range.map((page) => (
-                        <div className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer">
-                            {page}
+                    range.map((pagee) => (
+                        <div className={(pagee === page) ? active : inactive} onClick={() => handlePage(pagee)}>
+                            {pagee}
                         </div>
                     ))
                 }
--- src/features/filter/filterSlice.js
+++ src/features/filter/filterSlice.js
@@ -13,24 +13,32 @@ const filterSlice = createSlice({
     reducers: {
         searchTag: (state, action) => {
             state.searchTag = action.payload;
+            state.page = 1;
         },
         tagSelected: (state, action) => {
             state.tags.push(action.payload);
+            state.page = 1;
         },
         tagUnselected: (state, action) => {
             state.tags = state.tags.filter(tag => tag !== action.payload);
+            state.page = 1;
         },
         searchByAuthor: (state, action) => {
             state.searchAuthor = action.payload;
+            state.page = 1;
+        },
+        pageChanged: (state, action) => {
+            state.page = action.payload;
         },
         resetSearch: (state) => {
             state.searchTag = "";
             state.tags = [];
             state.searchAuthor = '';
+            state.page = 1;
         }
     },
 
 });
 
 export default filterSlice.reducer;
-export const { searchTag, tagSelected, tagUnselected, searchByAuthor, resetSearch } = filterSlice.actions;
+export const { searchTag, tagSelected, tagUnselected, searchByAuthor, resetSearch, pageChanged } = filterSlice.actions;
